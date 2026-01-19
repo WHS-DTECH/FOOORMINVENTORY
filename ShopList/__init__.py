2f1710544e1c3dc6d733f145ad64427f8a856fbc
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2026-01-19 15:50:40 +1300

    Resize and move Generate Shopping List button, remove duplicate Selected Bookings block

--- ShopList/__init__.py
+++ ShopList/__init__.py
@@ -1,13 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from . import shoppinglist
 
 shoplist_bp = Blueprint('shoplist', __name__, template_folder='templates')
-
 
 # Route for the new menu item 'Book the Shopping'
 @shoplist_bp.route('/book_the_shopping')
 def book_the_shopping():
-    """Serve the new Shopping List UI for the Book the Shopping menu item."""
-    bookings = []
-    recipes = []
-    dates = []
-    return render_template('shoplist_new.html', bookings=bookings, recipes=recipes, dates=dates)
+    """Serve the new Shopping List UI for the Book the Shopping menu item with week navigation."""
+    # Get week offset from query param, default to 0 (this week)
+    week_offset = 0
+    if 'week' in request.args:
+        try:
+            week_offset = int(request.args.get('week', 0))
+        except ValueError:
+            week_offset = 0
+    week_dates = shoppinglist.get_week_dates(week_offset)
+    week_label = shoppinglist.get_week_label(week_dates)
+    grid = shoppinglist.get_dummy_grid(week_dates)
+    # For demo, bookings and recipes are static; replace with real data as needed
+    bookings = [
+        {'class': '300HOSP', 'recipe': 'Chocolate Chip Cookies', 'period': 3, 'date': week_dates[1]},
+        {'class': 'SDFOOD', 'recipe': 'Apple and Sultana Crumble', 'period': 4, 'date': week_dates[3]},
+        {'class': '300HOSP', 'recipe': 'Cauliflower Cheese', 'period': 4, 'date': week_dates[4]},
+    ]
+    return render_template(
+        'shoplist_new.html',
+        week_label=week_label,
+        week_dates=week_dates,
+        grid=grid,
+        bookings=bookings,
+        week_offset=week_offset
+    )
