e7545bb2355a1147bc9aecf19c2466055074899a
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2026-01-19 14:50:29 +1300

    Restore debug_parser folder and all its contents

---
+++ debug_parser/debug_parser_URL.py
@@ -0,0 +1,12 @@
+# debug_parser_URL.py
+"""
+Parser strategies and confirmation logic for the Source URL block of a recipe.
+"""
+
+def confirm_source_url(raw_url):
+    """
+    Confirm and return the cleaned/validated source URL.
+    For now, this is a placeholder that simply returns the input.
+    """
+    # TODO: Add URL validation/cleaning logic here
+    return raw_url
