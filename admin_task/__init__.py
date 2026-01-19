989cde806474639a048aebefec517291fc0f33bc
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2026-01-19 22:36:25 +1300

    Set template_folder for admin_task blueprint so Flask can find admin_task templates

--- admin_task/__init__.py
+++ admin_task/__init__.py
@@ -1,7 +1,7 @@
 
 from flask import Blueprint
 
-admin_task_bp = Blueprint('admin_task', __name__)
+admin_task_bp = Blueprint('admin_task', __name__, template_folder='templates/admin_task')
 
 # Import all admin routes and utilities here
 from . import admin_routes
