e2c520141b0e98ed8d2df78f80268c3aca0adbb5
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2026-01-22 12:07:42 +1300

    program_help

--- program_help/program_help.py
+++ program_help/program_help.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 program_help_bp = Blueprint('program_help', __name__, template_folder='templates')
 
 @program_help_bp.route('/program_help')
-def help_home():
-    return render_template('program_help/help_home.html')
+def program_help():
+    return render_template('program_help/program_help.html')
