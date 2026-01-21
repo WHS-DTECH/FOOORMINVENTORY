840b3f68fdf52b9bdc4b16a6bbdf6593bbce8e86
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2026-01-22 11:00:05 +1300

    Add new route for recipe_source to display recipe source details by recipe_id

--- recipe_book/recipe_book.py
+++ recipe_book/recipe_book.py
@@ -1,3 +1,15 @@
+# --- Recipe Source Details Page ---
+@recipe_book_bp.route('/recipe_source/<int:recipe_id>')
+@require_login
+def recipe_source(recipe_id):
+    with get_db_connection() as conn:
+        c = conn.cursor()
+        c.execute('SELECT * FROM recipes WHERE id = %s', (recipe_id,))
+        recipe = c.fetchone()
+    if not recipe:
+        flash('Recipe not found.', 'error')
+        return redirect(url_for('recipe_book.recbk'))
+    return render_template('recipe_source.html', recipe=recipe)
 import json
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, session
 from flask_login import login_required, current_user
