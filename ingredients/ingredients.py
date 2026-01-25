0277aaa7cd771757400c261c688f75e01a36df7a
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2026-01-25 13:16:06 +1300

    Fix: Delete All button for ingredients and correct form action endpoint

--- ingredients/ingredients.py
+++ ingredients/ingredients.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from auth import require_role, get_db_connection
 
 ingredients_bp = Blueprint('ingredients', __name__, template_folder='templates')
@@ -12,3 +12,13 @@ def ingredients_page():
         c.execute('SELECT * FROM ingredient_inventory ORDER BY ingredient_name ASC')
         ingredients = c.fetchall()
     return render_template('ingredients/ingredients.html', ingredients=ingredients)
+
+@ingredients_bp.route('/ingredients/delete_all', methods=['POST'])
+@require_role('Admin')
+def delete_all_ingredients():
+    with get_db_connection() as conn:
+        c = conn.cursor()
+        c.execute('DELETE FROM ingredient_inventory')
+        conn.commit()
+    flash('All ingredients deleted.', 'success')
+    return redirect(url_for('ingredients.ingredients_page'))
